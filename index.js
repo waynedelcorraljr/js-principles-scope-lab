a2db0a7d1c3a03bc686b92a89cd75c5a8c2024b3
Wayne del Corral Jr <wdelcorjr@gmail.com>
2021-02-05 21:41:46 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+const leastFavoriteCustomer = 'Sean'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'still Sean'
+}
